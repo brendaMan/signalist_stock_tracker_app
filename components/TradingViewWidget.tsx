b0ff322c5fa76299600a50dd4a0729faefcb51f5
Brenda Manrique <manrique@arche-it.de>
2026-02-04 14:56:49 +0100

    Implement stock details page

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,27 +1,26 @@
-'use client'
-import React, {useRef, memo } from 'react';
+'use client';
+
+import React, { memo } from 'react';
 import useTradingViewWidget from "@/hooks/useTradingViewWidget";
 import {cn} from "@/lib/utils";
 
 interface TradingViewWidgetProps {
     title?: string;
-    scriptURL: string;
-    config: Record <string, unknown>;
+    scriptUrl: string;
+    config: Record<string, unknown>;
     height?: number;
     className?: string;
 }
 
-const TradingViewWidget = ({title, scriptURL, config, height = 600, className}: TradingViewWidgetProps) => {
-    const containerRef = useTradingViewWidget(scriptURL, config, height);
-
-
+const TradingViewWidget = ({ title, scriptUrl, config, height = 600, className }: TradingViewWidgetProps) => {
+    const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
     return (
         <div className="w-full">
             {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
-        <div className={cn("tradingview-widget-container", className)} ref={containerRef}>
-            <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
-        </div>
+            <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
+                <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
+            </div>
         </div>
     );
 }
